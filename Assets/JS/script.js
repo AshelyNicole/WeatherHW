5c0c2789f9b64b93b7312f3d684433b39ceaa8a2
Ashely <arumbaoa10@gmail.com>
2020-06-23 07:56:29 -0700

    updated weatherdashboard

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,86 +1,90 @@
-var weather = {};
-var location = "";
+$(document).ready(function(){
+   var weather = {};
+   
+   
+   //using user's geolocation
+   navigator.geolocation.getCurrentPosition(success, error)
+   function success(position) {
+      callWeatherAPI("", position.coords.latitude,position.coords.longitude)
+      isUserLocation = true
+   }
 
-$("#searchBtn").click(function()
-   location = $("#locationInput").val()
-   console.log("Give me a location", location)
-   var weatherURL =`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=f56c94699b79bf806441d23eacbfa401`
-   $.get(weatherURL, function(param){
-      console.log("Success!")
-      weather.name = param.name
-      weather.temp = param.main.temp
-      weather.humidity = param.main.humidity
-      weather.wind = param.wind.speed
-      weather.img = param.weather[0].icon
-      console.log("weather", weather)
-      $("#cityTitle").text(weather.name)
-      $("#cityTemp").text(weather.temp)
-      $("#cityHumidity").text(weather.humidity)
-      $("#citySpeed").text(weather.wind)
-      $("#weatherImg").attr("src", `http://openweathermap.org/img/wn/${weather.icon}.png`)
-   })
+   function error() {
+      $("#locationInput").show()
+      $("#searchBtn").show()
+   }
+   
+
+
+   function callWeatherAPI (inputLocation, lat, long) {
+      var weatherURL = "https://api.openweathermap.org/data/2.5/weather?"
+      var apiKey = "&units=imperial&appid="+ "&appid=4f4c69be324442d2253185e95d65f6bb"
+      var queryURL = ""
+      var currentLocation = "q=" + inputLocation
+      var currentLat = "lat=" + lat
+      var currentLong = "lon=" + long
+      
+      if(inputLocation =="") {
+         queryURL = weatherURL + currentLat + "&" + currentLong + apiKey
+      } else {
+         queryURL = weatherURL + currentLocation + apiKey
+      }
+      $.ajax({
+         url: queryURL,
+         method: "GET",
+         success: function(response) {
+            console.log("Success!")
+            weather.name = response.name
+            weather.temp = response.main.temp
+            weather.humidity = response.main.humidity
+            weather.wind = response.wind.speed
+            weather.img = response.weather[0].icon
+            console.log("weather", weather)
+            $("#cityTitle").text(weather.name)
+            $("#cityTemp").text(weather.temp)
+            $("#cityHumidity").text(weather.humidity)
+            $("#citySpeed").text(weather.wind)
+            $("#weatherImg").attr("src", `http://openweathermap.org/img/wn/${weather.icon}2x.png`)
+            $("#searchBtn").show()
+         }
+      })  
+   }
+
+   function uvIndex(inputLocation, lat, long) {
+      var weatherURL = "https://api.openweathermap.org/data/2.5/weather?"
+      var apiKey = "&units=imperial&appid="+ "&appid=4f4c69be324442d2253185e95d65f6bb"
+      var queryURL = ""
+      var currentLocation = "q=" + inputLocation
+      var currentLat = "lat=" + lat
+      var currentLong = "lon=" + long
+      
+      if(inputLocation =="") {
+         queryURL = weatherURL + currentLat + "&" + currentLong + apiKey
+      } else {
+         queryURL = weatherURL + currentLocation + apiKey
+      }
+      $.ajax({
+         url: queryURL,
+         type: "GET",
+         dataType: "json",
+         success: function(data) {
+            var uvIndex = $("#cityUV")
+            var uvBtn =$("<span>").addClass("btn btn-sm").text(data.value)
+
+            if(data.value < 3) {
+               uvBtn.addClass("btn-success")
+            } else if (data.value <7) {
+               uvBtn.addClass("btn-warning")
+            } else {
+               uvBtn.addClass("btn-danger")
+            }
+            
+            $("#uvlevel-item").append(uvIndex.append(uvBttn))
+
+         }
+      })
+   }
 })
 
 
 
-    
-
-
-// navigator.geolocation.getCurrentPosition (success, error)
-// if success(function(param){
-//     var latitude = "lat=" + position.coords.latitude
-//     var longitude = "lon=" + position.coords.longitude
-//     var geoURL = "https://api.openweathermap.org/data/2.5/weather?"
-//     var APIKey = "&appid=f56c94699b79bf806441d23eacbfa401";
-//     var queryURL = geoURL + latitude + "&" + longitude +APIKey
-//     $.get(queryUrl, function(param){
-//         weather = param
-//         location = param.name
-//         countryCode = param.sys.countryCode
-//         $("#searchBtn").show()
-//     }
-// })
-
-// if error(){
-//     $("#searchBtn").click(function(){
-//         location = $("#locationInput").val()
-//         console.log("Give me a location", location)
-//         var weatherURL =`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=f56c94699b79bf806441d23eacbfa401`
-//         $.get(weatherURL, function(param){
-//             console.log("Success!")
-//             weather.name = param.name
-//             weather.temp = param.main.temp
-//             weather.humidity = param.main.humidity
-//             weather.wind = param.wind.speed
-//             weather.img = param.weather[0].icon
-//             console.log("weather", weather)
-//         })
-//     })
-    
-    
-//     // Current Weather 
-//     $("#cityTitle").text(weather.name)
-//     $("#cityTemp").text(weather.temp)
-//     $("#cityHumidity").text(weather.humidity)
-//     $("#citySpeed").text(weather.wind)
-//     $("#weatherImg").attr("src", `http://openweathermap.org/img/wn/${weather.icon}.png`)
-}
-    
-    
-
-
-
-
-
-//Five Day Forecast
-// var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&appid=${APIKey}`
-
-// $("#forecastbtn").click(function(){
-//     $.get(forecastURL, function(param){
-//         weather.name = param.name
-//         weather.temp = param.main.temp
-//         weather.humidity = param.main.humidity
-//         weather.wind = param.wind.speed
-//         weather.img = param.weather[0].icon
-//     })
-// })
